e296206f83f9f58f44a5f94e6f3e410be1f386d5
Troels Østbjerg <troelsostbjerg@gmail.com>
2026-05-24 22:06:40 +0200

    Add Troels candidate profile

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://sitedokai.com',
   integrations: [tailwind()],
+  devToolbar: {
+    enabled: false,
+  },
   build: {
     inlineStylesheets: 'always',
   },
